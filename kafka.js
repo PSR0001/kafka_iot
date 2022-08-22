7a02552097a9534036ad4a41961d0d248d265d85
CoolD69 <parthasingharoy166@gmail.com>
2022-08-22 08:49:25 +0530

    all ok

--- kafka.js
+++ kafka.js
@@ -1,4 +1,5 @@
 const { Kafka } = require('kafkajs')
+require('dotenv').config()
 
 const { KAFKA_USERNAME: username, KAFKA_PASSWORD: password } = process.env
 const sasl = username && password ? { username, password, mechanism: 'plain' } : null
@@ -7,7 +8,7 @@ const ssl = !!sasl
 // This creates a client instance that is configured to connect to the Kafka broker provided by
 // the environment variable KAFKA_BOOTSTRAP_SERVER
 const kafka = new Kafka({
-  clientId: 'npm-slack-notifier',
+  clientId: 'connector-producer-lcc-xqvqdz-0',
   brokers: [process.env.KAFKA_BOOTSTRAP_SERVER],
   ssl,
   sasl
--- server.js
+++ server.js
@@ -1,6 +1,24 @@
+process.binding('http_parser').HTTPParser = require('http-parser-js').HTTPParser;
+
+
 const createHookReceiver = require('npm-hook-receiver')
+require('dotenv').config()
+const kafka = require('./kafka')
+
+const producer = kafka.producer()
+// const topic = process.env.TOPIC
+const admin = kafka.admin()
 
 const main = async () => {
+  //connect the kafka Producer
+  await producer.connect()
+  await admin.connect()
+
+    // await admin.createTopics({
+    //   topics: [{ topic }],
+    //   waitForLeaders: true,
+    // })
+  
   const server = createHookReceiver({
     // Secret created when registering the webhook with NPM.
     // Used to validate the payload. 
@@ -12,6 +30,29 @@ const main = async () => {
 
   server.on('package:publish', async event => {
     // Send message to Kafka
+
+    try {
+      const responses = await producer.send({
+        topic: topic_0,
+        messages: [{
+          // Name of the published package as key, to make sure that we process events in order
+          key: event.name,
+  
+          // The message value is just bytes to Kafka, so we need to serialize our JavaScript
+          // object to a JSON string. Other serialization methods like Avro are available.
+          value: JSON.stringify({
+            package: event.name,
+            version: event.version
+          })
+        }]
+      })
+  
+      console.log('Published message', { responses })
+    } catch (error) {
+      console.error('Error publishing message', error)
+    }
+
+
   })
 
   server.listen(process.env.PORT || 3000, () => {
